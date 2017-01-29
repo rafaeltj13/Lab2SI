b77b1a8298acdf4d89df656a1b32689abc57f5c4
rafaeltj13 <rafael.maciel@ccc.ufcg.edu.br>
2017-01-29 18:13:11 -0300

    add: tela de edição

--- js/controller/toDoListCtrl.js
+++ js/controller/toDoListCtrl.js
@@ -10,7 +10,64 @@ angular.module("toDoList", []).controller("toDoListCtrl", function ($scope) {
 		{description:"Fazer o Lab"},
 		{description:"Estudar"},
 		{description:"Fazer exercícios"}
-		];
+	];
+
+	myScope.taskList = [
+		{
+			description:"Fazer o Lab",
+			tasks: [
+					{
+						taskDescription: "Front end",
+						subTasks: [
+							{subTaskDescription:"Html"},
+							{subTaskDescription:"CSS"}
+							]
+					},
+					{
+						taskDescription:"Back end",
+						subTasks: [
+							{subTaskDescription:"Spring"}
+						]
+					}
+			]
+		},
+		{
+			description:"Estudar",
+			tasks: [
+					{
+						taskDescription:"Estudar lógica",
+						subTasks: [
+							{subTaskDescription:"Proposicional"},
+							{subTaskDescription:"Condicional"}
+						]
+					},
+					{
+						taskDescription:"Estudar SI",
+						subTasks: [
+							{subTaskDescription:"Angular"},
+							{subTaskDescription:"Spring"}
+						]
+					}
+			]
+		}
+	];
+
+	myScope.addTaskList = function (taskListName) {
+		var newTaskList = {description: taskListName, tasks: []};
+		myScope.taskList.push(newTaskList);
+		delete myScope.newTaskList.description;
+	};
+
+	myScope.addTask = function (task, list) {
+		var newTask = {taskDescription: task, subTasks: []};
+		list.tasks.push(newTask);
+		//delete task.taskDescription;
+	};
+
+	myScope.addSubTask = function (subTask, task) {
+		var newSubTask = {subTaskDescription:subTask};
+		task.subTasks.push(newSubTask);
+	};
 
 	myScope.adicionarTarefa = function (task) {
 		myScope.tasks.push(angular.copy(task));
